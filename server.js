bbc47cfb8fa32bd57f07652df8db4a70f7b6aafa
mf618872@gmail.com <fm618872@gmail.com>
2026-04-18 23:46:50 +0500

    Fix CORS issue

--- server.js
+++ server.js
@@ -9,7 +9,16 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: [process.env.FRONTEND_URL, "http://localhost:5173"], credentials: true }));
+app.use(cors({ 
+  origin: [
+    process.env.FRONTEND_URL, 
+    "http://localhost:5173",
+    "https://final-frontend-three-peach.vercel.app"
+  ], 
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 dataBase();
